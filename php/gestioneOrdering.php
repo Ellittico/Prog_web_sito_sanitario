9b09a65386f103912b392e6a283455eff6d8c385
Ellittico <elisa.arnoldi.ea@gmail.com>
2024-06-12 14:31:15 +0200

    Update gestioneOrdering.php

--- php/gestioneOrdering.php
+++ php/gestioneOrdering.php
@@ -5,36 +5,37 @@ if (empty($_SESSION['file'])) {
 
 switch ($_SESSION['file']) {
     case "/prog-sito-sanitario-final/pages/cittadini.php":
+        $selectedOption = $_SESSION['order'] ?? ''; // Ottieni l'opzione selezionata precedentemente, se esiste
         ?>
-        <option class="sort-bar-option" value="A-Z">Nome Alfabetico A-Z</option>
-        <option class="sort-bar-option" value="Z-A">Nome Alfabetico Z-A</option>
-        <option class="sort-bar-option" value="Crescente">Cognome Alfabetico A-Z</option>
-        <option class="sort-bar-option" value="Decrescente">Cognome Alfabetico Z-A</option>
+        <option class="sort-bar-option" value="A-Z" <?php echo ($selectedOption == 'A-Z') ? 'selected' : ''; ?>>Nome Alfabetico A-Z</option>
+        <option class="sort-bar-option" value="Z-A" <?php echo ($selectedOption == 'Z-A') ? 'selected' : ''; ?>>Nome Alfabetico Z-A</option>
+        <option class="sort-bar-option" value="Crescente" <?php echo ($selectedOption == 'Crescente') ? 'selected' : ''; ?>>Cognome Alfabetico A-Z</option>
+        <option class="sort-bar-option" value="Decrescente" <?php echo ($selectedOption == 'Decrescente') ? 'selected' : ''; ?>>Cognome Alfabetico Z-A</option>
         <?php
         break;
-    case "/prog-sito-sanitario-final/pages/ospedale.php": 
+    case "/prog-sito-sanitario-final/pages/ospedale.php":
+        $selectedOption = $_SESSION['order'] ?? ''; // Ottieni l'opzione selezionata precedentemente, se esiste 
         ?>
-        <option class="sort-bar-option" value="A-Z">Citta Alfabetico A-Z</option>
-        <option class="sort-bar-option" value="Z-A">Citta Alfabetico Z-A</option>
-        <option class="sort-bar-option" value="Crescente">Codice Alfabetico A-Z</option>
-        <option class="sort-bar-option" value="Decrescente">Codice Alfabetico Z-A</option>
-        <?php
+        <option class="sort-bar-option" value="A-Z" <?php echo ($selectedOption == 'A-Z') ? 'selected' : ''; ?>>Citta Alfabetico A-Z</option>
+        <option class="sort-bar-option" value="Z-A" <?php echo ($selectedOption == 'Z-A') ? 'selected' : ''; ?>>Citta Alfabetico Z-A</option>
+        <option class="sort-bar-option" value="Crescente" <?php echo ($selectedOption == 'Crescente') ? 'selected' : ''; ?>>Codice Alfabetico A-Z</option>
+        <option class="sort-bar-option" value="Decrescente" <?php echo ($selectedOption == 'Decrescente') ? 'selected' : ''; ?>>Codice Alfabetico Z-A</option>        <?php
         break;
-    case "/prog-sito-sanitario-final/pages/patologia.php": 
+    case "/prog-sito-sanitario-final/pages/patologia.php":
+        $selectedOption = $_SESSION['order'] ?? ''; // Ottieni l'opzione selezionata precedentemente, se esiste 
         ?>
-        <option class="sort-bar-option" value="A-Z">Alfabetico A-Z</option>
-        <option class="sort-bar-option" value="Z-A">Alfabetico Z-A</option>
-        <option class="sort-bar-option" value="Crescente">Criticità Crescente</option>
-        <option class="sort-bar-option" value="Decrescente">Criticità Decrescente</option>
-        <?php
+       <option class="sort-bar-option" value="A-Z" <?php echo ($selectedOption == 'A-Z') ? 'selected' : ''; ?>>Alfabetico A-Z</option>
+        <option class="sort-bar-option" value="Z-A" <?php echo ($selectedOption == 'Z-A') ? 'selected' : ''; ?>>Alfabetico Z-A</option>
+        <option class="sort-bar-option" value="Crescente" <?php echo ($selectedOption == 'Crescente') ? 'selected' : ''; ?>>Criticità Crescente</option>
+        <option class="sort-bar-option" value="Decrescente" <?php echo ($selectedOption == 'Decrescente') ? 'selected' : ''; ?>>Criticità Decrescente</option>        <?php
         break;
     case "/prog-sito-sanitario-final/pages/ricovero.php": 
+        $selectedOption = $_SESSION['order'] ?? ''; // Ottieni l'opzione selezionata precedentemente, se esiste
         ?>
-        <option class="sort-bar-option" value="A-Z">Durata Crescente</option>
-        <option class="sort-bar-option" value="Z-A">Durata Decrescente</option>
-        <option class="sort-bar-option" value="Crescente">Costo Crescente</option>
-        <option class="sort-bar-option" value="Decrescente">Costo Decrescente</option>
-        <?php
+        <option class="sort-bar-option" value="A-Z" <?php echo ($selectedOption == 'A-Z') ? 'selected' : ''; ?>>Durata Crescente</option>
+        <option class="sort-bar-option" value="Z-A" <?php echo ($selectedOption == 'Z-A') ? 'selected' : ''; ?>>Durata Decrescente</option>
+        <option class="sort-bar-option" value="Crescente" <?php echo ($selectedOption == 'Crescente') ? 'selected' : ''; ?>>Costo Crescente</option>
+        <option class="sort-bar-option" value="Decrescente" <?php echo ($selectedOption == 'Decrescente') ? 'selected' : ''; ?>>Costo Decrescente</option>        <?php
         break; 
 
     default: break;
